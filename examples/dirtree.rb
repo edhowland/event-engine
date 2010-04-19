a4c79d190dffc78bdc756f7e462aa2fbc1e64aef
Ed Howland <ed.howland@gmail.com>
2010-04-19 17:48:00 -0400

    examples/dirtree.rb uses Pathname prior to branch

--- examples/dirtree.rb
+++ examples/dirtree.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
+require "pathname"
 require "event-engine"
 
 eng = EventEngine::Engine.new
 
 eng.setup do |en|
+  # en.on_file {|ev| }
   en.handle ->(ev) {puts ev.to_s}
 end
 
 # walk tree triggering events for each node
-Dir['../**/*'].each {|l| eng.trigger l}
+Dir['../**/*'].each {|l| eng.trigger Pathname.new(l)}
